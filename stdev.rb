a8835d63599dac2d50390efa14f532a55ddf9483
Alex <meetalexjohnson@gmail.com>
2017-03-17 13:03:27 -0700

    standard deviation script 2.0

--- stdev.rb
+++ stdev.rb
@@ -1,10 +1,16 @@
-samples = [5,2,3,4,1]
-sampleLength = samples.length
-sampleMean = samples.inject(0, :+) / sampleLength
-deviations = []
+def sum_of_array(arr)
+    arr.inject(0, :+)
+end
 
-deviations = samples.map { |x| (x - sampleMean) ** 2 }
-totalOfDeviations = deviations.inject(0, :+)
-standard_deviation = Math.sqrt(totalOfDeviations / sampleLength) 
+def standard_deviation(samples)
+    sampleLength = samples.length
+    sampleMean = sum_of_array(samples) / sampleLength
+    deviations = samples.map { |x| (x - sampleMean) ** 2 }
+    standard_deviation = Math.sqrt(sum_of_array(deviations) / sampleLength) 
 
-puts standard_deviation
+    puts standard_deviation
+end
+
+samples = ARGV[0].split(",").map { |s| s.to_i }
+
+standard_deviation(samples)
