b3da52708e7284098be2fb64fb58d174d55f0664
Alex <meetalexjohnson@gmail.com>
2017-03-17 12:44:11 -0700

    standard deviation script 1.0

--- stdev.rb
+++ stdev.rb
@@ -1,15 +1,10 @@
-def calculateIndividualDeviation(x)
-    (x - sampleMean) ** 2
-end
+samples = [5,2,3,4,1]
+sampleLength = samples.length
+sampleMean = samples.inject(0, :+) / sampleLength
+deviations = []
 
-def stdev
-    samples = [5,2,3,4,1]
-    sampleLength = samples.length
-    sampleMean = samples.inject(0, :+) / sampleLength
-    deviations = []
+deviations = samples.map { |x| (x - sampleMean) ** 2 }
+totalOfDeviations = deviations.inject(0, :+)
+standard_deviation = Math.sqrt(totalOfDeviations / sampleLength) 
 
-    deviations = samples.map { |x| calculateIndividualDeviation(x) }
-    totalOfDeviations = deviation.inject(0, :+)
-    standard_deviation = Math.sqrt(deviation / sampleLength) 
-end
-
+puts standard_deviation
